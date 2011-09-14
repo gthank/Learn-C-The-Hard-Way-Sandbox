f61c10e20851f4d6294fe305b2fe6b6c92b8e524
Hank Gay <whg@us.ibm.com>
2011-09-13 23:15:45 -0400

    Add back C++-style comment.

--- ex5.c
+++ ex5.c
@@ -4,6 +4,7 @@
 int main(/*@unused@*/ int argc, /*@unused@*/ char *argv[]) {
     int distance = 100;
 
+    // This is also a comment.
     printf("You are %d miles away.\n", distance);
 
     return 0;
